c920f7ccbe893c23d091cd1863d59b8fc42c6e48
Carlos Eduardo <carlosestudos799@gmail.com>
2025-10-14 14:03:54 -0300

    Update exercicio13.php

--- exercicio13.php
+++ exercicio13.php
@@ -5,8 +5,8 @@ Disciplina: Linguagem e Técnicas de Programação
 Professor: Flores
 Turma: ESOFT-2A
 Componentes:
- 25001132-2 - Cauã Chaerki Bobato
  25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
  25161143-2 - Gabriel Hikari Uchida Requião 
  25229817-2 - Guilherme Garcia Da Cruz 
  25201449-2 - Luan Raio Amorim 
@@ -15,7 +15,7 @@ Componentes:
  25178371-2 - Pedro Henrique Santos Sinhuk
 
 Data: 08 de Outubro de 2025
-Descritivo: verificador de senha
+Descritivo: Descrição da atividade feita
 *******************************************************************************/
 do {
     $senhaDigitada = readline("Digite a senha: ");
