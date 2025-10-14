e30c8ca2f2943d2821f08e46ed212983af9afee7
Carlos Eduardo <carlosestudos799@gmail.com>
2025-10-14 14:07:37 -0300

    Create exercicio14.php

---
+++ exercicio14.php
@@ -0,0 +1,28 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 08 de Outubro de 2025
+Descritivo: Leitura de números até valor negativo
+*******************************************************************************/
+do {
+    $numeroInformado = (int)readline("Digite um número: ");
+    if ($numeroInformado >= 0) {
+        echo "Você digitou: $numeroInformado\n";
+    }
+} while ($numeroInformado >= 0);
+
+echo "Número negativo detectado. Encerrando o programa.\n";
+?>
