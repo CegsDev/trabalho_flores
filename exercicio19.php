9ebcafe5484d6bbc920490be2a50322df1260dc9
maikonkm <maikonvinicio@gmail.com>
2025-10-16 16:15:26 -0300

    Add files via upload

---
+++ exercicio19.php
@@ -0,0 +1,7 @@
+<?php
+// Cria um array com 10 números inteiros
+$numeros = [5, 12, 8, 21, 1, 99, 42, 7, 15, 30];
+
+// Soma todos os valores do array usando array_sum() e exibe o resultado
+echo "A soma de todos os números é: " . array_sum($numeros);
+?>
