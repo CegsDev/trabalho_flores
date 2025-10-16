a5e6fe0bdb80150b576c14c1e6abadef68f1f05c
maikonkm <maikonvinicio@gmail.com>
2025-10-16 17:04:04 -0300

    Update exercicio23.php

--- exercicio23.php
+++ exercicio23.php
@@ -1,3 +1,21 @@
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 16 de Outubro de 2025
+Descritivo: Descrição da atividade feita
+*******************************************************************************/
 <?php
 
 // (array associativo)
@@ -43,4 +61,5 @@ foreach ($produtos as $produto) {
 echo "\nTotal de todos os produtos: R$ " . number_format($total, 2, ',', '.') . "\n";
 
 ?>
+
 
