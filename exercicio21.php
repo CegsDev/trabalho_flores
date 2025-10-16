af67bfa8e349bf5fc15bbd92b0788cf8e09ccb15
maikonkm <maikonvinicio@gmail.com>
2025-10-16 16:20:42 -0300

    Update exercicio21.php

---
+++ exercicio21.php
@@ -0,0 +1,12 @@
+<?php
+$aluno = array(
+    "nome" => "Maikon Duarte",
+    "idade" => 20,
+    "curso" => "Engenharia de Software"
+);
+
+// Exibindo os dados
+echo "Nome: " . $aluno["nome"] . "\n";
+echo "Idade: " . $aluno["idade"] . "\n";
+echo "Curso: " . $aluno["curso"] . "\n";
+?>
