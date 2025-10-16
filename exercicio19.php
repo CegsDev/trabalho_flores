56081c198b6dbf1fd65b0760f914168fd0738065
maikonkm <maikonvinicio@gmail.com>
2025-10-16 17:02:06 -0300

    Update exercicio19.php

--- exercicio19.php
+++ exercicio19.php
@@ -1,7 +1,26 @@
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 16 de Outubro de 2025
+Descritivo: 
+*******************************************************************************/
 <?php
 // Cria um array com 10 números inteiros
 $numeros = [5, 12, 8, 21, 1, 99, 42, 7, 15, 30];
 
 // Soma todos os valores do array usando array_sum() e exibe o resultado
 echo "A soma de todos os números é: " . array_sum($numeros);
+
 ?>
