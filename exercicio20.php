88bb9c8df694ace392e41e0a4c7fc4a6419bd277
maikonkm <maikonvinicio@gmail.com>
2025-10-16 16:20:22 -0300

    Update exercicio20.php

---
+++ exercicio20.php
@@ -0,0 +1,9 @@
+<?php
+// Array com os 7 dias da semana
+$dias_da_semana = array("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
+
+// Laço for para imprimir os dias
+for ($i = 0; $i < count($dias_da_semana); $i++) {
+    echo $dias_da_semana[$i] . "\n";
+}
+?>
