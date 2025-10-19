33f421873f4cdf5e2ed3d45ef32c28d556ea938e
Carlos Eduardo <carlosestudos799@gmail.com>
2025-10-19 19:38:16 -0300

    Add course details and city list to exercicio16.php
    Added a header comment with course details and a list of cities.

---
+++ exercicio16.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 11 de Outubro de 2025
+Descritivo: lista de cidades
+*******************************************************************************/
+// 16. Lista de cidades
+$cidades = ["Fortaleza", "Recife", "Natal", "João Pessoa", "Maceió"];
+print_r($cidades);
+?>
--- exericicio16.php
+++
@@ -1,5 +0,0 @@
-<?php
-// 16. Lista de cidades
-$cidades = ["Fortaleza", "Recife", "Natal", "João Pessoa", "Maceió"];
-print_r($cidades);
-?>
