5ac521670397077d233afc8225165c52e2391279
Carlos Eduardo <carlosestudos799@gmail.com>
2025-10-19 19:35:41 -0300

    Add script to display prime numbers from 1 to 50
    This script displays prime numbers from 1 to 50.

---
+++ exercicio15.php
@@ -0,0 +1,37 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 08 de Outubro de 2025
+Descritivo: exibe os numeros primos de 1 a 50
+*******************************************************************************/
+
+
+for ($numeroAtual = 2; $numeroAtual <= 50; $numeroAtual++) {
+    $ehPrimo = true;
+
+    for ($divisor = 2; $divisor <= sqrt($numeroAtual); $divisor++) {
+        if ($numeroAtual % $divisor == 0) {
+            $ehPrimo = false;
+            break;
+        }
+    }
+
+    if ($ehPrimo) {
+        echo "$numeroAtual ";
+    }
+}
+echo "\n";
+?>
