dbe09db39c58edf1205008dd233b398a29aad40f
cauacbobato <cauacbobato@gmail.com>
2025-10-21 14:40:44 -0300

    Implement number printing from 1 to 10
    Add header comments and a for loop to print numbers.

--- exercicio01.php
+++ exercicio01.php
@@ -1,1 +1,24 @@
+<?php 
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
 
+Data: 14 de Outubro de 2025
+Descritivo: Imprimir os números de 1 a 10 com for
+***************************/
+
+for ($n = 1; $n <= 10; $n++) {
+    echo $n . "\n";
+}
+?>
