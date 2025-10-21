63d94e812caec2b95db456485165735179ded99e
cauacbobato <cauacbobato@gmail.com>
2025-10-21 14:43:32 -0300

    Update and rename exercicio5.php to exercicio05.php

---
+++ exercicio05.php
@@ -0,0 +1,27 @@
+<?php 
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 19 de Outubro de 2025
+Descritivo:  Calcular e imprimir a soma dos números de 1 a 100 com for
+***************************/
+
+$result = 0;
+for ($a = 1; $a <= 100; $a++) {
+    $result += $a;
+}
+echo "Soma de 1 a 100 - Resultado: $result\n"
+
+?>
