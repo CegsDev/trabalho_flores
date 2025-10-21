187912a5766fcf079925201c575b077e2c955ef1
gbuchida <gabrieluchidacontato@gmail.com>
2025-10-21 18:21:30 -0300

    Add imprimirArray function to display array contents
    Implement function to print array elements.

---
+++ exercicio35.php
@@ -0,0 +1,34 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25119616-2 -Lucas Henrique Zeferino
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+25178371-2 - Pedro Henrique Santos Sinhuk
+25229817-2 - Guilherme Garcia Da Cruz 
+25201449-2 - Luan Raio Amorim 
+
+Data: 08 de Outubro de 2025
+Descritivo: Descrição da atividade feita
+***************************/
+function imprimirArray($array) {
+    if (!is_array($array)) {
+        echo "O valor informado não é um array.\n";
+        return;
+    }
+
+    foreach ($array as $elemento) {
+        echo $elemento . "\n";
+    }
+}
+
+// Aray vai aqui;
+$meuArray = [10, 20, 30, 40, 50];
+
+imprimirArray($meuArray);
+?>
