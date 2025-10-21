5364c8e9299732efd80f89d395cdc819333ac26e
cauacbobato <cauacbobato@gmail.com>
2025-10-21 14:41:37 -0300

    Update and rename exercicio3.php to exercicio03.php

---
+++ exercicio03.php
@@ -0,0 +1,24 @@
+<?php 
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 14 de Outubro de 2025
+Descritivo: Imprimir a tabuada do 5, de 1 a 10 com for
+***************************/
+
+for ($m = 1; $m <= 10; $m++) {
+    echo "5 x $m = " . (5 * $m) . "\n";
+}
+?>
