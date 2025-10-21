e27719db51530609b26472b88039c89c74ea01cd
cauacbobato <cauacbobato@gmail.com>
2025-10-21 14:41:09 -0300

    Add script to print even numbers from 2 to 20
    This script prints even numbers from 2 to 20 using a for loop.

---
+++ exercicio02.php
@@ -0,0 +1,24 @@
+<?php 
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25001132-2 - Cauã Chaerki Bobato
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25229817-2 - Guilherme Garcia Da Cruz 
+ 25201449-2 - Luan Raio Amorim 
+ 25119616-2 - Lucas Henrique Zeferino
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25178371-2 - Pedro Henrique Santos Sinhuk
+
+Data: 14 de Outubro de 2025
+Descritivo: Imprimir os números pares de 2 a 20 com for
+***************************/
+
+for ($w = 2; $w <= 20; $w += 2) {
+    echo $w . "\n";
+}
+?>
