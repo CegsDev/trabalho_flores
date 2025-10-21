582ec337735c570ee9d49a4de574fb0c2d28151c
gbuchida <gabrieluchidacontato@gmail.com>
2025-10-21 18:32:26 -0300

    Update exercicio32.php

---
+++ exercicio32.php
@@ -0,0 +1,25 @@
+<?php    
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25119616-2 -Lucas Henrique Zeferino
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+25178371-2 - Pedro Henrique Santos Sinhuk
+25229817-2 - Guilherme Garcia Da Cruz 
+25201449-2 - Luan Raio Amorim 
+
+Data: 08 de Outubro de 2025
+Descritivo: Crie um procedimento saudacaoUsuario que receba um nome como parâmetro e imprima "Olá, [nome]!".
+***************************/
+function saudacaoUsuario($nome) {
+    echo "Olá, $nome!";
+}
+
+// Exemplo de uso:
+saudacaoUsuario("Matheus");
+?>
