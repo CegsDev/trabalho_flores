21aa9e24a1ae049129d66b2d69040d1c8201aa74
gbuchida <gabrieluchidacontato@gmail.com>
2025-10-21 18:22:59 -0300

    Update exercicio36.php

---
+++ exercicio36.php
@@ -0,0 +1,25 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25119616-2 -Lucas Henrique Zeferino
+ 25148436-2 - Carlos Eduardo Galdino Sousa 
+ 25165588-2 - Maikon V. Duarte dos Santos
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+25178371-2 - Pedro Henrique Santos Sinhuk
+25229817-2 - Guilherme Garcia Da Cruz 
+25201449-2 - Luan Raio Amorim 
+
+Data: 08 de Outubro de 2025
+Descritivo: Crie um procedimento desenharLinha que imprima uma linha de 20 hifens --------------------.
+***************************/
+function desenharLinha() {
+    echo str_repeat("-", 20) . "\n";
+}
+
+
+desenharLinha();
+?>
